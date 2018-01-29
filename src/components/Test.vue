f3222b8b8d688b66ed6fcb6b28368c354c4122cb
rukaroa <rukaroa@gmail.com>
2018-01-29 08:56:03 -0600

    object return test

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="test">
       <h1>
-          TEST
+          {{title}}
       </h1>
+      <p>{{user.first_name}}</p>
+      <p v-text="user.last_name"/>
   </div>
 </template>
 
 <script>
     export default {
-        name: "test"
+        name: "test",
+        data(){
+            return{
+                title: 'Hello World',
+                user: {
+                    first_name: "john",
+                    last_name: "doe"
+                }
+            }            
+        }
     }
 </script>
 
