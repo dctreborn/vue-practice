c30389bee6b3074d2bec9c9d6170d303ef932c02
rukaroa <rukaroa@gmail.com>
2018-01-29 08:00:38 -0600

    test component created

---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+      <h1>
+          TEST
+      </h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "test"
+    }
+</script>
+
+<style scoped>
+
+</style>
