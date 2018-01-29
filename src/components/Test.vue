ce40366a0fb5a30b66eb7475b3d23c52e76b391d
rukaroa <rukaroa@gmail.com>
2018-01-29 10:22:15 -0600

    if else example added

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -3,7 +3,8 @@
       <h1>
           {{title}}
       </h1>
-      <p>{{user.first_name}}</p>
+      <p v-if="showName">{{user.first_name}}</p>
+      <p v-else="showName">Gud</p>
       <p v-text="user.last_name"/>
       <span v-html="html"/>
   </div>
@@ -19,7 +20,8 @@
                     first_name: "john",
                     last_name: "doe"
                 },
-                html: 'V-test'
+                html: 'V-test',
+                showName: false
             }            
         }
     }
