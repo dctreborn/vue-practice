5df95e59de3cd86cac960fc37080970f015d258c
rukaroa <rukaroa@gmail.com>
2018-01-30 07:59:23 -0600

    basic add user form example

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,12 +1,60 @@
 <template>
   <div class="users">
-        
+        <h1>Users</h1>
+
+        <form v-on:submit="addUser">
+            <input type="text" v-model="newUser.name" placeholder="enter name"> <br/>
+            <input type="text" v-model="newUser.email" placeholder="enter email"> <br/>
+            <button type="submit">Submit</button>
+        </form>
+
+        <ul>
+            <li v-for="user in users">
+                {{user.name}}: {{user.email}}
+            </li>
+        </ul>
   </div>
 </template>
 
 <script>
     export default {
-        name: "users"
+        name: "users",
+        data() {
+            return {
+                newUser: {},
+                users: [
+                    {
+                        name: "John Doe",
+                        email: "jdoe@g.com",
+                        contacted: false
+                    },
+                    {
+                        name: "Steve",
+                        email: "steve@g.com",
+                        contacted: false
+                    },
+                    {
+                        name: "Grea",
+                        email: "grea@g.com",
+                        contacted: false
+                    }
+                ]
+            }
+        },
+        methods: {
+            addUser: function (e) {
+                e.preventDefault();
+                this.users.push({
+                    name: this.newUser.name,
+                    email: this.newUser.email,
+                    contacted: false
+                });
+                console.log("add");
+            }
+        },
+        computed: {
+
+        }
     }
 </script>
 
